0988f0b2a6db08c44b4fcce8d64bf7ebed26d29c
André Schmitz <andre.schmitz@lise.de>
2022-11-10 17:32:47 +0100

    Project is setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-primary h-full w-full box-content">
+      <button className={"text-base btn btn-primary text-2xl"}>spps</button>
+    </div>
+  );
+}
+
+export default App;
