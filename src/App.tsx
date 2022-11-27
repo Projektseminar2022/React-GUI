70e04eb6cd7fe45e452c220d81009fbe959d6df0
André Schmitz <andre.schmitz@lise.de>
2022-11-27 21:54:08 +0100

    React router implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Lander from "./App/Lander";
 
 export default function App() {
   return (
     <>
       <div className="h-full w-full min-h-screen">
-          <div className={"m-4"}>
-              <Navbar/>
-
+        <div className={"m-4"}>
+          <Navbar/>
+          <div className={"mt-2"}>
+            <BrowserRouter>
+              <Routes>
+                <Route path={"/"} element={<Lander/>}/>
+              </Routes>
+            </BrowserRouter>
           </div>
+        </div>
       </div>
     </>
   );
