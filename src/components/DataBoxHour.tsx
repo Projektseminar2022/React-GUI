f106bce34a4a7b2e16ae790fc23747101f38509b
Levin Hennig <levinhennig@gmx.de>
2022-12-01 15:28:51 +0100

    Added Hero and databox

---
+++ src/components/DataBoxHour.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DataBoxHour() {
+    return (
+        <div>
+            <div className="divider"></div>
+        {/*  see: https://i.imgur.com/WOCsOrK.png   */}
+        </div>
+
+    );
+}
